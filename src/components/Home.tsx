1ff1bfa8e7de2cbcddb9aa328ef19481fa60f124
Srivathsa Yashwanth <srivathsayashwanth.vavillashanmugam@iqvia.com>
2024-11-17 15:31:48 +0530

    add mui

---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+import Paper from "@mui/material/Paper";
+import Card from "@mui/material/Card";
+
+import styles from "../styles/home.module.css";
+
+const Home = () => {
+  return (
+    <Paper className={styles.homeBackground}>
+      <Card>
+        <h1>Spot Mix</h1>
+        
+      </Card>
+    </Paper>
+  );
+};
+
+export default Home;
